23d8dcb48b443cbaf66503329f67df0b745bf5f7
Robin JOSEPH <robin82joseph@gmail.com>
2023-03-04 05:22:41 +0100

    firebase_core package added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({key});
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
