450262976473851cebea7f2df1091753f87f8197
Robin JOSEPH <robin82joseph@gmail.com>
2023-03-04 05:53:43 +0100

    Connexion to database is working !

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,12 +15,24 @@ void main() async {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  void writeData() async {
+    DatabaseReference ref = FirebaseDatabase.instance.ref("questions/2");
+
+    await ref.set({
+      "blue_thumb": 0,
+      "id": 2,
+      "red_thumb": 0,
+      "text": "Mon comptable rêve de bible et de gros chiffres."
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child:
+              TextButton(onPressed: writeData, child: const Text("Write data")),
         ),
       ),
     );
