35f8c8baf0d3bdc84aaf1e2d069331076432d639
Robin JOSEPH <49043100+robjo82@users.noreply.github.com>
2023-03-24 00:18:45 +0100

    Just 2 imports than I delete during the merging

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:chat_chuispt/textstyle.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 
 import 'main_page.dart';
 
