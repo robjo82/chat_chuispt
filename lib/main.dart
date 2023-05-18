b9da13e4eda0c79cec3483361c0af588c23f9a72
Robin <rjrdutg@gmail.com>
2023-05-18 18:52:25 +0200

    first try for google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:chatchuispt/src/repositories/authentication/user_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,17 +27,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// Home page which just allow to sign in with Google, display the current user and sign out
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,72 +37,71 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// State of the home page
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // User repository
+  final UserRepository _userRepository = UserRepository();
 
-  void _incrementCounter() {
+  // Current user
+  User? _currentUser;
+
+  // Method to sign in with Google
+  Future<void> _signInWithGoogle() async {
+    final User? user = await _userRepository.signInWithGoogle();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentUser = user;
     });
   }
 
+  // Method to sign out
+  Future<void> _signOut() async {
+    await _userRepository.signOut();
+    setState(() {
+      _currentUser = null;
+    });
+  }
+
+  // Method to get the current user
+  Future<void> _getCurrentUser() async {
+    final User? user = _userRepository.getCurrentUser();
+    setState(() {
+      _currentUser = user;
+    });
+  }
+
+  // Build the home page
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            // Sign in button
+            ElevatedButton(
+              onPressed: _signInWithGoogle,
+              child: const Text('Sign in with Google'),
             ),
+            // Current user
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Current user: ${_currentUser?.displayName ?? 'None'}',
+            ),
+            // Sign out button
+            ElevatedButton(
+              onPressed: _signOut,
+              child: const Text('Sign out'),
+            ),
+            // Get current user button
+            ElevatedButton(
+              onPressed: _getCurrentUser,
+              child: const Text('Get current user'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/src/repositories/authentication/user_repository.dart
+++ lib/src/repositories/authentication/user_repository.dart
@@ -1,1 +1,67 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/foundation.dart';
+
+// Classe pour gérer les interactions avec les utilisateurs
+class UserRepository {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  // Méthode pour se connecter avec Google
+  Future<User?> signInWithGoogle() async {
+    if (kDebugMode) {
+      print("signInWithGoogle");
+    }
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+          await _googleSignIn.signIn();
+      if (googleSignInAccount != null) {
+        final GoogleSignInAuthentication googleSignInAuthentication =
+            await googleSignInAccount.authentication;
+
+        final AuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: googleSignInAuthentication.accessToken,
+          idToken: googleSignInAuthentication.idToken,
+        );
+
+        final UserCredential userCredential =
+            await _auth.signInWithCredential(credential);
+        final User? user = userCredential.user;
+
+        if (user != null) {
+          // Vérifier si l'utilisateur est nouveau ou existe déjà
+          final isNewUser = userCredential.additionalUserInfo!.isNewUser;
+          if (isNewUser) {
+            if (kDebugMode) {
+              print("New user created");
+            }
+            return user;
+          }
+        }
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      return null;
+    }
+    return null;
+  }
+
+  // Méthode pour obtenir l'utilisateur actuel
+  User? getCurrentUser() {
+    if (kDebugMode) {
+      print("getCurrentUser ${_auth.currentUser}");
+    }
+    return _auth.currentUser;
+  }
+
+  // Méthode pour se déconnecter
+  Future<void> signOut() async {
+    if (kDebugMode) {
+      print("signOut");
+    }
+    await _auth.signOut();
+    await _googleSignIn.signOut();
+  }
+}
